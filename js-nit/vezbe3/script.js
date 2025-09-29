5b366806d521e8eb06d2e4c0923cb8148cdef74b
bimmer.boyy <hasmaci123>
2025-09-29 21:08:35 +0200

    map,filter,reduce exercise

--- js-nit/vezbe3/script.js
+++ js-nit/vezbe3/script.js
@@ -7,7 +7,7 @@ const numbers = [1, 2, 3, 4, 5];
 const squaredNumbers = numbers.map((num)=>{
     return num * 2;
 });
-
+console.log("1.Niz od kvadriranih brojeva:")
 console.log(squaredNumbers);
 
 //2. Imas niz stringova,pretvori ih sve u velika slova.Koristi toUpperCase
@@ -17,6 +17,7 @@ const toUpperCase = strings.map((str) =>{
    return str.toUpperCase();
 });
 
+console.log("2.Niz od velikih slova")
 console.log(toUpperCase);
 
 //FILTER metoda
@@ -27,6 +28,7 @@ const parni = numbers2.filter((num) =>{
     return (num % 2 === 0);
 });
 
+console.log("3.Niz od parnih brojeva");
 console.log(parni);
 //4.Iz datog niza izdvoj samo reci koje imaju vise od 3 slova
 const words = ["sun","mountian","tree","river","sky"];
@@ -36,11 +38,12 @@ const threeWords = words.filter((word) => {
     
 });
 
+console.log("4.Niz od reci koje imaju vise od 3 slova");
 console.log(threeWords);
-
+/*
 //5. Izracunaj zbir svih parnih brojeva u nizu
 
-const brojevi = [3,-1,7,2,-5,8];
+
 
 const parni2 = brojevi.filter((num) =>{
     return (num % 2 === 0);
@@ -54,8 +57,20 @@ console.log(zbirParnih);
 
 //5. II nacin
 
-const zbirParnih2 = brojevi.reduce((total,br) =>{
-    return br % 2 === 0 ? total + br : total;
+const zbirParnih2 = brojevi.reduce((akumulator,br) =>{
+    return br % 2 === 0 ? akumulator + br : akumulator; 
 },0);
 
 console.log(zbirParnih2);
+*/
+
+//5.Izracunati pozitive brojeve
+
+const brojevi = [3,-1,7,2,-5,8];
+
+const zbirPozitivnih = brojevi.reduce((akumulator,br) =>{
+    return (br > 0) ? akumulator + br : akumulator
+},0);
+
+console.log("5.Suma pozitivnih brojeva");
+console.log(zbirPozitivnih);
