05e2afce80a2a199abf051143665a801d1869b76
bimmer.boyy <hasmaci123>
2026-01-23 20:38:14 +0100

    binding

--- js-nit/vezbe-26/script.js
+++ js-nit/vezbe-26/script.js
@@ -1,37 +1,82 @@
-class Playlist{
-    constructor(name){
-        this.name = name;
-        this.song = [];
+// class Playlist{
+//     constructor(name){
+//         this.name = name;
+//         this.song = [];
+//     }
+//     addSong(song){
+//         this.song.push(song);
+//     }
+//     removeSong(song){
+//         let index = this.song.indexOf(song);
+//         this.song.splice(index,1);
+//     }
+//     getSongCount(){
+//         return this.song.length;
+//     }
+//     showPlaylist(){
+//         console.log(`Playlist:${this.name}\n\n`);
+//         this.song.forEach(s =>{
+//             console.log('-' + s);
+//         });
+//     }
+// }
+
+// let playlist = new Playlist('Plejlista');
+
+// playlist.addSong('pesma1');
+// playlist.addSong('pesma2');
+// playlist.addSong('pesma3');
+// playlist.addSong('pesma4');
+// playlist.addSong('pesma5');
+
+// playlist.removeSong('pesma3');
+// playlist.removeSong('pesma2');
+
+// console.log(playlist.song);
+
+// playlist.showPlaylist();
+
+
+//STA NE RADI
+/*
+console.log('Direktan poziv');
+btn.click(); //Kliknuto na:Submit
+
+//Ovo ne radi - metoda dovojena od objekta
+console.log('\nOdvojena funkcija');
+const clickFn = btn.click;
+//cickFn - Ovo bi dalo "Kliknuto na: undifined"
+console.log('{clickFn() bi dao undifined - this je izgubljen');
+
+*/
+
+class Counter {
+    constructor(broj){
+        this.broj = broj;
+        this.intervalId = null;
     }
-    addSong(song){
-        this.song.push(song);
+
+    increment(){
+        this.broj++;
+        console.log(this.broj);
     }
-    removeSong(song){
-        let index = this.song.indexOf(song);
-        this.song.splice(index,1);
+
+    startAutoIncrement(milliseconds = 1000){
+        this.intervalId = setInterval(
+            this.increment.bind(this),
+            milliseconds
+        );
     }
-    getSongCount(){
-        return this.song.length;
-    }
-    showPlaylist(){
-        console.log(`Playlist:${this.name}\n\n`);
-        this.song.forEach(s =>{
-            console.log('-' + s);
-        });
+    stopAutoIncrement(){
+        if(this.broj > 20){
+             clearInterval(this.intervalId);
+        }
+       
     }
 }
 
-let playlist = new Playlist('Plejlista');
+let counter = new Counter(1);
+counter.startAutoIncrement(1000); // 1 sekunda
 
-playlist.addSong('pesma1');
-playlist.addSong('pesma2');
-playlist.addSong('pesma3');
-playlist.addSong('pesma4');
-playlist.addSong('pesma5');
+counter.stopAutoIncrement();
 
-playlist.removeSong('pesma3');
-playlist.removeSong('pesma2');
-
-console.log(playlist.song);
-
-playlist.showPlaylist();
